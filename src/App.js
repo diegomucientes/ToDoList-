fda476cb94c1ae7b4d6982ebfa5b9d097ef90d14
Rodrigo Orellana <rodrigo.orelana@gmail.com>
2021-05-01 11:58:35 -0400

    agrega componente TaskCreation para crear tareas

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import TaskCreation from './Components/TaskCreation';
-import ShowTask from './Components/ShowTask';
-
-
+import React, {useState} from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import TaskCreation from './Components/TaskCreation'
+import ShowTask from './Components/ShowTask'
 
 function App() {
+  const [tareas, setTareas] = useState([]); 
   
-  return (
-    <div className="App">
-        <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link exact to="/show-task"> Show Task</Link>
-            </li>
-            <li>
-              <Link exact to="/create-task">Crear Task</Link>
-            </li>
-            <li>
-              <Link to="edit-task/:id">Editar Task</Link>
-            </li>
-          </ul>
-        </nav>
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/show-task">
+                                    {' '}
+                                    Show Task
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/create-task">
+                                    Crear Task
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="edit-task/:id">Editar Task</Link>
+                            </li>
+                        </ul>
+                    </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+                    {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/show-task">
-            <ShowTask/>
-          </Route>
-          <Route path="/create-task">
-            <TaskCreation/>
-          </Route>
-          <Route path="/edit-task/:id">
-            <TaskCreation/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+                    <Switch>
+                        <Route path="/show-task">
+                            <ShowTask />
+                        </Route>
+                        <Route path="/create-task">
+                            <TaskCreation tareas={tareas} setTareas={setTareas}/>
+                        </Route>
+                        <Route path="/edit-task/:id">
+                            <TaskCreation />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    )
 }
 
-
-      
-    </div>
-  );
-}
-
-export default App;
+export default App
--- src/Components/TaskCreation.js
+++ src/Components/TaskCreation.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useState}  from 'react';
 
-const TaskCreation = () => {
+const TaskCreation = (props) => {
+    
+    //console.log(props)
+    const {setTareas, tareas} = props
+    const [tarea, setTarea] = useState()
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+/*         const form = e.target;
+        const formData = new FormData(form);
+        const payload = {};
+        for (const [key, val] of formData.entries()) payload[key] = val;
+        const tareaNueva = payload['ftarea']; */
+
+        setTareas(
+            previousState => 
+                [...previousState, tarea]  
+        )
+    
+    }
+    console.log(tareas)
+    const onChange = (e) => {
+        setTarea(e.target.value)
+    }
+
     return (
-        <div>
-            
-            
-        </div>
+        
+        <form onSubmit={onSubmit}>
+            <label htmlFor="ftarea">Tarea:</label>
+            <input type="text" id="ftarea" name="ftarea" onChange={onChange}/>
+            <button>Enviar</button>
+        </form>
     );
 }
 
